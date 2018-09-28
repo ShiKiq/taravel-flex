d07a935ae47e90e83dd470399437e2f563269408
unknown <B6130@oic.osaka.jp>
2018-09-28 17:58:08 +0900

    おおおtuika

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelですよ。
+                    Laravelですよ。おおおおおお
                 </div>
 
                 <div class="links">
