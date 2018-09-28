97abc8421150c8b694ee274c96743bf04f5e95ea
unknown <B6130@oic.osaka.jp>
2018-09-28 17:50:46 +0900

    toppageの内容を変更しました

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelですよ。
                 </div>
 
                 <div class="links">
