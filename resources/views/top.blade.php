f43bcdc1ea2acf7164c9aa57a8d6554291ba0505
vagrant <vagrant@homestead>
2018-10-16 09:01:52 +0000

    テストでtopつくってる

---
+++ resources/views/top.blade.php
@@ -0,0 +1,23 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>OICツアー</h1>
+    {{--<img src="../img/umi.jpg" alt="umim">--}}
+
+
+
+    <div style="float:left;">
+        <img src="../img/umi.jpg"width="640" height="360"/>
+    </div>
+    <div style="float:left;">
+        <img src="../img/umi2.jpg" width="640" height="360"/>
+    </div>
+    <div style="float:left;">
+        <img src="../img/umi3.jpg"width="640" height="360"/>
+    </div>
+    <br clear="both"/>
+
+
+@endsection
