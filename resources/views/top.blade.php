961ca6b02cd9da45f4a18a90474581b78e91a9fe
vagrant <vagrant@homestead>
2018-11-02 04:59:57 +0000

    ２かいめ

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -2,22 +2,48 @@
 @extends('layouts.app')
 
 @section('content')
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 
-    <h1>OICツアー</h1>
-    {{--<img src="../img/umi.jpg" alt="umim">--}}
 
+<div class="box1">
+    <a>気が付くとすでに地球を飛び立っていた</a>
+</div>
 
+    <div class="boxA">
+        <div class="box2">
 
     <div style="float:left;">
-        <img src="../img/umi.jpg"width="640" height="360"/>
+        <div class="example">
+            <img src="../img/umi.jpg"width="640" height="360"/>
+            <i class="fa fa-youtube" ></i>
+
+        </div>
     </div>
+
+        </div>
+        <div class="box3">
+
     <div style="float:left;">
         <img src="../img/umi2.jpg" width="640" height="360"/>
     </div>
-    <div style="float:left;">
+
+        </div>
+        <div class="box4">
+
+        <div style="float:left;">
         <img src="../img/umi3.jpg"width="640" height="360"/>
     </div>
+
+        </div>
+    </div>
+
     <br clear="both"/>
 
 
+    </div>
+    <div style="float;">
+    <a href="#" onclick="location.href='ここURL'" class="square_btn">ツアー一覧 </a>
+    </div>
 @endsection
