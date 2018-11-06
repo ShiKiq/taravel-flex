76d0e01530fbcc163eeab875e6e3e67810675106
vagrant <vagrant@homestead>
2018-11-06 05:22:32 +0000

    ichiran tukutta

---
+++ resources/views/top.blade.php
@@ -0,0 +1,27 @@
+
+
+<!doctype html>
+<html lang="ja">
+<html>
+<head>
+    <title>IC　topページ</title>
+</head>
+<body>
+<h1><center>OIC　topページ</center></h1>
+
+<table cellpadding="0" cellspacing="0"><tbody>
+    <tr>
+        <td>
+            <img src="img/a.jpg"width="650" height="650" />
+        </td>
+        <td>
+            <img src="img/aa.jpg"width="650" height="650" />
+        </td>
+        <td>
+            <img src="img/11.jpg"width="650" height="650" />
+        </td>
+    </tr>
+    </tbody></table>
+
+</body>
+</html>
