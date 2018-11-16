d2e7a131343a30542993f945c9ef5b98321de338
vagrant <vagrant@homestead>
2018-11-16 07:42:25 +0000

    tukutta

--- resources/views/ichiran.blade.php
+++ resources/views/ichiran.blade.php
@@ -8,16 +8,79 @@
     <meta http-equiv="content-language" content="ja">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href='/css/ichiran.css'>
-    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
     <title>oic　topページ</title>
 </head>
 <body>
 <h1><center>ツアー　一覧</center></h1>
-<p style="border: 4px solid red; background:#CCFFCC; color:purple;">
-    <body style="background: 	#DDFFFF; text-align: center;">
+<h3><center>都道府県</center></h3>
+
+    <p style="text-align:center;">
+<a href="/yoyaku"  class="hvr-rectangle-out">北海道</a>
+<a href="#" class="hvr-rectangle-out">青森県</a>
+<a href="#" class="hvr-rectangle-out">岩手県</a>
+<a href="#" class="hvr-rectangle-out">宮城県</a>
+<a href="#" class="hvr-rectangle-out">秋田県</a>
+<a href="#" class="hvr-rectangle-out">山形県</a>
+<a href="#" class="hvr-rectangle-out">福島県</a>
+<a href="#" class="hvr-rectangle-out">茨城県</a>
+<a href="#" class="hvr-rectangle-out">栃木県</a>
+<a href="#" class="hvr-rectangle-out">埼玉県</a>
+<a href="#" class="hvr-rectangle-out">千葉県</a>
+<a href="#" class="hvr-rectangle-out">東京都</a>
+<a href="#" class="hvr-rectangle-out">神奈川県</a>
+<a href="#" class="hvr-rectangle-out">新潟県</a>
+<a href="#" class="hvr-rectangle-out">富山県</a>
+<a href="#" class="hvr-rectangle-out">石川県</a>
+<a href="#" class="hvr-rectangle-out">福井県</a>
+<a href="#" class="hvr-rectangle-out">長野県</a>
+<a href="#" class="hvr-rectangle-out">岐阜県</a>
+<a href="#" class="hvr-rectangle-out">静岡県</a>
+</p>
+</body>
+
+<center>
+    <a href="#" class="hvr-rectangle-out">愛知県</a>
+    <a href="#" class="hvr-rectangle-out">三重県</a>
+    <a href="#" class="hvr-rectangle-out">滋賀県</a>
+    <a href="#" class="hvr-rectangle-out">京都府</a>
+    <a href="#" class="hvr-rectangle-out">大阪府</a>
+    <a href="#" class="hvr-rectangle-out">兵庫県</a>
+    <a href="#" class="hvr-rectangle-out">奈良県</a>
+    <a href="#" class="hvr-rectangle-out">和歌山県</a>
+    <a href="#" class="hvr-rectangle-out">鳥取県</a>
+    <a href="#" class="hvr-rectangle-out">島根県</a>
+    <a href="#" class="hvr-rectangle-out">岡山県</a>
+    <a href="#" class="hvr-rectangle-out">広島県</a>
+    <a href="#" class="hvr-rectangle-out">山口県</a>
+    <a href="#" class="hvr-rectangle-out">徳島県</a>
+    </center>
+
+
+<center>
+    <a href="#" class="hvr-rectangle-out">香川県</a>
+    <a href="#" class="hvr-rectangle-out">愛媛県</a>
+    <a href="#" class="hvr-rectangle-out">高知県</a>
+    <a href="#" class="hvr-rectangle-out">福岡県</a>
+    <a href="#" class="hvr-rectangle-out">佐賀県</a>
+    <a href="#" class="hvr-rectangle-out">長崎県</a>
+    <a href="#" class="hvr-rectangle-out">熊本県</a>
+    <a href="#" class="hvr-rectangle-out">大分県</a>
+    <a href="#" class="hvr-rectangle-out">宮崎県</a>
+    <a href="#" class="hvr-rectangle-out">鹿児島県</a>
+    <a href="#" class="hvr-rectangle-out">沖縄県</a>
+    </center>
+
+
+
+
 
 
     <h2><center>新着＆旬のおすすめ</center></h2>
+
+</body>
+
+
+
 <div class="contents-wrap">
     <div class="contents-box">
         <a href="index.html"><img src="/img/aa.jpg" alt=border="0"></a>
