15c17f635564f2075aecd5ad0dccdb78cd814d51
vagrant <vagrant@homestead>
2018-11-20 07:49:56 +0000

    dwadadwadwa

--- resources/views/ichiran.blade3.php
+++ resources/views/ichiran.blade3.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="content-language" content="ja">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href='/css/ichiran.css'>
+    <link rel="stylesheet" href='/css/ichiran3.css'>
     <title>oic　topページ</title>
 </head>
 <body>
